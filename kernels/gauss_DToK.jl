97736caa2ad5e586298833a2d97a1d65455dc0a2
Hadseth <guillaume.guex@gmail.com>
2017-06-01 16:52:27 +0200

    Add files via upload

---
+++ kernels/gauss_DToK.jl
@@ -0,0 +1,21 @@
+############################################################
+############ Gaussian Kernel
+############################################################
+
+
+gauss_DToK = function(D)
+  # size
+  n = size(D)[1]
+
+  # Compute the Gaussian Kernel
+  sig = sum(D)/(n*(n-1))
+  K = exp(- D ./sig)
+
+  # Normalising
+  Lam,U = eig(K)
+  Lam,U = real(Lam),real(U)
+  Lam[Lam.<0] = 0
+  K = U*diagm(Lam)*U'
+
+  return(K)
+end
